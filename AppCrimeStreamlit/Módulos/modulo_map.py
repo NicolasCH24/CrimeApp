b4304265045e2234c3cfb329e5ae8154202fcf81
Nicolas Chaparro <nchaparro@buenosaires.gob.ar>
2024-12-18 12:46:21 -0300

    Funciones agregadas de localización para el mapa.

--- AppCrimeStreamlit/Módulos/modulo_map.py
+++ AppCrimeStreamlit/Módulos/modulo_map.py
@@ -2,8 +2,19 @@
 import streamlit as st
 
 # MAPA
-import folium
+import folium as fl
+from streamlit_folium import st_folium
 
 def folium_map():
-    m = folium.Map(location=[-34.6083, -58.3712], zoom_start=12)
-    return m
+    m = fl.Map(location=[-34.6083, -58.3712], zoom_start=12)
+    m.add_child(fl.LatLngPopup())
+    map = st_folium(m, height=700, width='100%')
+    return map
+
+def get_location(lat, lon):
+    return lat, lon
+
+def get_lat_lon(map):
+    data = get_location(map['last_clicked']['lat'], map['last_clicked']['lng'])
+    return data
+
