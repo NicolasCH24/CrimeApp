ad3c82047eec8577cfe6b678f132b9a00d1058d1
Nicolas Chaparro <nchaparro@buenosaires.gob.ar>
2024-12-20 11:52:29 -0300

    Optimización del módulo de mapa.

--- AppCrimeStreamlit/app.py
+++ AppCrimeStreamlit/app.py
@@ -3,26 +3,31 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 # MODULO MAPA
-from Módulos.modulo_map import folium_map
+from Módulos.modulo_map import folium_map, container_map
     
 def main():
     #st.title("App BA Delito")
-
     with st.sidebar:
-        selected = option_menu('Menú', ['Mapa', 'Estadística'],
-            icons=['map', 'bar-chart'], menu_icon='cast', default_index=1)
+        selected = option_menu('Menú', ['Mapa', 'Estadística',"Tabla personal"],
+            icons=['map', 'bar-chart', 'table'], menu_icon='cast', default_index=0)
 
     if selected == 'Mapa':
-        col1, col2 = st.columns([5.3,20])
+        col1, col2 = st.columns([4, 10])
         with col1:
-            st.image("C:/Users/20391117579/Dropbox/CrimeApp/Multimedia/image.png", width=350)
+            st.image(
+                "C:/Users/20391117579/Dropbox/CrimeApp/Multimedia/image.png",  width=350)
         with col2:
             st.title("Ciudad Autónoma de Buenos Aires")
-        map = folium_map()
+
+    # CONTAINER MAP
+        m = folium_map()
+        modulo_mapa = container_map(m)
 
     elif selected == 'Estadística':
             st.subheader('Estadística')
             st.write("Estadística")
+
+    # CONTAINER ESTADISTICA
 
         
 if __name__ == "__main__":
