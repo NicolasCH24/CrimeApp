a11d34b0279e6951a116ebde29b0fef64d608421
Nicolas Chaparro <nchaparro@buenosaires.gob.ar>
2024-12-26 09:30:01 -0300

    Configuración de páguina al inicio.

--- AppCrimeStreamlit/app.py
+++ AppCrimeStreamlit/app.py
@@ -6,7 +6,11 @@ from streamlit_option_menu import option_menu
 from Módulos.modulo_map import folium_map, container_map
     
 def main():
-    #st.title("App BA Delito")
+    st.set_page_config(
+        page_title="App BA Delito",
+        page_icon=":chart_with_upwards_trend:",
+        layout="wide"
+    )
     with st.sidebar:
         selected = option_menu('Menú', ['Mapa', 'Estadística',"Tabla personal"],
             icons=['map', 'bar-chart', 'table'], menu_icon='cast', default_index=0)
@@ -31,9 +35,4 @@ def main():
 
         
 if __name__ == "__main__":
-    st.set_page_config(
-        page_title="App BA Delito",
-        page_icon=":chart_with_upwards_trend:",
-        layout="wide"
-    )
     main()
