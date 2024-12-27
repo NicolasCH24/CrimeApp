48bc78fba8afbbd692bd90a28a4301b12539590d
Nicolas Chaparro <nchaparro@buenosaires.gob.ar>
2024-12-27 14:22:11 -0300

    Nuevo formato de tabla de localización actual

--- AppCrimeStreamlit/Módulos/modulo_map.py
+++ AppCrimeStreamlit/Módulos/modulo_map.py
@@ -1,6 +1,9 @@
 # STREAMLIT
 import streamlit as st
 
+# DATOS
+import pandas as pd
+
 # MAPA - GRAFICOS
 import folium as fl
 from streamlit_folium import st_folium
@@ -56,124 +59,124 @@ class AddMarkerOnClick(MacroElement):
         super().__init__()
         self._name = 'AddMarkerOnClick'
 
+class ModuloMap:
+    def __init__(self):
+        self.clase_datos = Datos()
 
-def folium_map():
-    m = fl.Map(location=[-34.6083, -58.3712], zoom_start=12)
-    m.add_child(AddMarkerOnClick())
-    return m
+    # FUNCIONALIDADES
 
-def graph_table(lat, lon):
-    clase_datos = Datos()
-    df_data = clase_datos.get_current_location(lat, lon)
-    # TABLA
-    values = [['Fecha',
-            'Comuna',
-            'Barrio',
-            'Franja horaria',
-            'Zona',
-            'Índice de peligro - Zona']
-            , [df_data['Fecha'].values, df_data['Comuna'].values, df_data["Barrio"].values, df_data['Franja Horaria'].values, df_data['Zona'].values, df_data['ZonaPeligroIndice'].values]]
+    def folium_map(self):
+        m = fl.Map(location=[-34.6083, -58.3712], zoom_start=12)
+        m.add_child(AddMarkerOnClick())
+        return m
 
-    fig_table = go.Figure(data=[go.Table(
-    columnorder = [1,2],
-    columnwidth = [60,80],
-    header = dict(
-        values = [['<b>TABLA DELICTIVA</b><br>para el dia de hoy'],
-                    ['<b>DESCRIPCIÓN</b>']],
-        line_color='#929795',
-        fill_color='black',
-        align=['left','center'],
-        font=dict(color='white', size=12),
-        height=40
-    ),
-    cells=dict(
-        values=values,
-        line_color='#929795',
-        fill=dict(color=['gray', 'white']),
-        align=['left', 'center'],
-        font=dict(color="black"),
-        font_size=12,
-        height=30)
-        )
-    ])
-    fig_table.update_layout(margin=dict(t=0, l=0, r=0, b=0))
-    return fig_table
-
-def graph_mapa(lat, lon):
-    fig_map = go.Figure(go.Scattermapbox(
-    lat=[lat],
-    lon=[lon],
-    mode='markers',
-    ))
-
-    # Configurar el layout del mapa
-    fig_map.update_layout(
-        margin=dict(t=0, l=0, r=0, b=0),
-        mapbox_style="open-street-map",  # Estilo del mapa
-        mapbox_zoom=14,  # Nivel de zoom
-        mapbox_center={"lat": lat, "lon": lon},  # Centro del mapa
-    )
-    return fig_map
-
-def dashboard(fig_table, fig_map):
-    col1 = st.columns([1])
-    with col1[0]:
-        st.markdown("Mapa del delito")
-    
-    col2_1, col2_2 = st.columns([3, 10])
-    with col2_1:
-        with st.container(border=True):
-            st.plotly_chart(fig_table, use_container_width=True)
-    with col2_2:
-        with st.container(border=True):
-            st.plotly_chart(fig_map, use_container_width=True)
-
-def container_map(m):
-    # DATOS GLOBALES
-    clase_datos = Datos()
-    _df = clase_datos.get_df_by_query(query="""
-                                        SELECT
-                                        FECHA,
-                                        FRANJA_HORARIA,
-                                        CONTACTO_ID
-                                        FROM
-                                        FCT_HECHOS
-                                        WHERE YEAR(FECHA) = (SELECT MAX(YEAR(FECHA)) FROM FCT_HECHOS)
-                                        GROUP BY
-                                        FECHA, FRANJA_HORARIA, CONTACTO_ID
-                                        ORDER BY
-                                        FECHA;
-                                        """)
-    df_table = clase_datos.get_data_table(_df)
-
-    # SESSIONS STATES
-    if "location" not in st.session_state:
-        st.session_state.location = None
-
-    col1, col2 = st.columns([10, 10])
-    with col1:
-        with st.container(border=True):
-            destino = st.text_input("Ingresá destino aquí o agregar marcador en el mapa.")
-            franja_horaria = st.slider( "Seleccioná en qué momento del día vas a concurrir.",
-                                            value=(time(11,30)))
-            styled_df = df_table.style.background_gradient(
-                cmap="YlOrBr", axis=None
+    def graph_table(self, lat, lon):
+        df_table = self.clase_datos.get_actual_location_table(lat, lon)
+        barrio = df_table["Barrio"].values[0]
+        barrio = barrio.upper()
+        tabla = st.dataframe(
+                df_table,
+                width=1000,
+                column_config={
+                    barrio: st.column_config.LineChartColumn(
+                        "Contactos por mes",
+                        help="Historial de contactos mensuales",
+                        y_min=0,
+                    ),
+                    "Franja horaria": st.column_config.LineChartColumn(
+                        "Contactos por franja horaria",
+                        help="Historial de contactos por franja horaria",
+                        y_min=0,
+                    ),
+                },
+                hide_index=True,
             )
-            st.dataframe(styled_df, use_container_width=True, on_select="ignore")
+        
+        return tabla
+ 
+    def graph_mapa(self, lat, lon):
+        fig_map = go.Figure(go.Scattermapbox(
+        lat=[lat],
+        lon=[lon],
+        mode='markers',
+        ))
 
-    with col2:
+        # Configurar el layout del mapa
+        fig_map.update_layout(
+            margin=dict(t=0, l=0, r=0, b=0),
+            mapbox_style="open-street-map",  # Estilo del mapa
+            mapbox_zoom=14,  # Nivel de zoom
+            mapbox_center={"lat": lat, "lon": lon},  # Centro del mapa
+        )
+        return fig_map
+
+    # DASHBOARD
+    def dashboard(self, tabla, fig_map):
+        col1 = st.columns([1])
+        with col1[0]:
+            st.markdown("Mapa del delito")
+        
         with st.container(border=True):
-            map = st_folium(m, width="%100", height=500)
-            if map.get("last_clicked"):
-                st.session_state.location = [map["last_clicked"]["lat"], map["last_clicked"]["lng"]]
-                with st.expander("Información"):
-                    if st.session_state["location"] == [map["last_clicked"]["lat"], map["last_clicked"]["lng"]]:
-                        fig_table = graph_table(lat=map["last_clicked"]["lat"], lon=map["last_clicked"]["lng"])
-                        fig_map = graph_mapa(lat=map["last_clicked"]["lat"], lon=map["last_clicked"]["lng"])
-                        dashboard(fig_table, fig_map)
-                        st.session_state.location = None
+            tabla
+        col1, col2 = st.columns([4, 10])
+        with col2:
+            with st.container(border=True):
+                st.plotly_chart(fig_map, use_container_width=True)
 
-## MEJORAR SPANDER CON ST.DATAFRAME Y UN MINMAP DEL BARRIO/ZONA EN CUESTION CON ALGUN FORMATO CONDICIONAL
+    # CONTAINER
+    def container_map(self, m):
+        # DATOS GLOBALES
+        _df = self.clase_datos.get_df_by_query(query="""
+                                            SELECT
+                                            FECHA,
+                                            FRANJA_HORARIA,
+                                            CONTACTO_ID
+                                            FROM
+                                            FCT_HECHOS
+                                            WHERE YEAR(FECHA) = (SELECT MAX(YEAR(FECHA)) FROM FCT_HECHOS)
+                                            GROUP BY
+                                            FECHA, FRANJA_HORARIA, CONTACTO_ID
+                                            ORDER BY
+                                            FECHA;
+                                            """)
+        df_table = self.clase_datos.get_data_table(_df)
 
-## CREAR CLASE DE MODULO MAPA EN ESTE ARCHIVO --- PRIMERO LAS FUNCIONES --- SEGUNDO EL CONTAINER
+        # SESSIONS STATES
+        if "prev_location" not in st.session_state:
+            st.session_state.prev_location = None
+
+        if "dashboard" not in st.session_state:
+            st.session_state.dashboard = None
+
+        col1, col2 = st.columns([10, 5.5])
+        with col1:
+            with st.container(border=True):
+                destino = st.text_input("Ingresá destino aquí o agregar marcador en el mapa.")
+                franja_horaria = st.slider("Seleccioná en qué momento del día vas a concurrir.",
+                                        value=(time(11, 30)))
+                styled_df = df_table.style.background_gradient(cmap="YlOrBr", axis=None)
+                st.markdown("Grilla horaria - Último año")
+                tabla_grilla = st.dataframe(styled_df, use_container_width=True, hide_index=False)
+
+        with col2:
+            with st.container(border=True, height=663):
+                map = st_folium(m, width="%100", height=620, use_container_width=False)
+                if map.get("last_clicked"):
+                    lat = map["last_clicked"]["lat"]
+                    lon = map["last_clicked"]["lng"]
+                    current_location = [lat, lon]
+                else:
+                    current_location = None
+
+        with st.expander("Información"):
+            tabla = self.graph_table(lat, lon)
+            fig_map = self.graph_mapa(lat, lon)
+            if st.session_state.prev_location != current_location:
+                st.session_state.prev_location = current_location
+                dashboard = self.dashboard(tabla, fig_map)
+            else:
+                dashboard = self.dashboard(tabla, fig_map)
+
+## RESOLVER ACTUALIZACION CADA QUE SE INTERACTUA CON EL MAPA, VER DE ALMANCENAR EN CACHE Y RESSTABLECERLO AL CAMBIAR COORDENADAS SOLAMENTE.
+
         
