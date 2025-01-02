899c7b51bdfb58dbb39fca0d3d8fc5a42d07ff94
Nicolas Chaparro <nchaparro@buenosaires.gob.ar>
2025-01-02 16:03:22 -0300

    Adaptación main app para el mapa y dashboard.

--- AppCrimeStreamlit/app.py
+++ AppCrimeStreamlit/app.py
@@ -7,7 +7,7 @@ from Módulos.modulo_map import ModuloMap
     
 def main():
     modulo_mapa = ModuloMap()
-    st.set_page_config(
+    configuracion_pag = st.set_page_config(
         page_title="App BA Delito",
         page_icon=":chart_with_upwards_trend:",
         layout="wide"
@@ -17,6 +17,10 @@ def main():
             icons=['map', 'bar-chart', 'table'], menu_icon='cast', default_index=0)
 
     if selected == 'Mapa':
+        # SESSIONS STATES
+        if "selected_location" not in st.session_state:
+            st.session_state.selected_location = None
+
         col1, col2 = st.columns([4, 10])
         with col1:
             st.image(
@@ -26,7 +30,15 @@ def main():
 
     # CONTAINER MODULO MAPA
         m = modulo_mapa.folium_map()
-        modulo_mapa = modulo_mapa.container_map(m)
+        if st.session_state.selected_location:
+            if st.button("Regresar"):
+                mapa = modulo_mapa.container_map(m)
+                st.session_state.selected_location = None
+            else:
+                dashboard = modulo_mapa.container_dashboard(_lat=st.session_state.selected_location[0], _lon=st.session_state.selected_location[1])
+        else:
+            mapa = modulo_mapa.container_map(m)
+
 
     elif selected == 'Estadística':
             st.subheader('Estadística')
@@ -36,3 +48,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+### MEJORAR DASHBOARD
