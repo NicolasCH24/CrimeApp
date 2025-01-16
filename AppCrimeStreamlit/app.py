224ba9ac61c85591244c0086b8f5ca26245b437f
Nicolas Chaparro <nchaparro@buenosaires.gob.ar>
2025-01-16 10:14:39 -0300

    Page config error, resuelto.

--- AppCrimeStreamlit/app.py
+++ AppCrimeStreamlit/app.py
@@ -11,44 +11,38 @@ st.set_page_config(
         layout="wide"
     )
     
-def main():
-    modulo_mapa = ModuloMap()
+modulo_mapa = ModuloMap()
 
-    with st.sidebar:
-        selected = option_menu('Menú', ['Mapa', 'Estadística',"Tabla personal"],
-            icons=['map', 'bar-chart', 'table'], menu_icon='cast', default_index=0)
+with st.sidebar:
+    selected = option_menu('Menú', ['Mapa', 'Estadística',"Tabla personal"],
+        icons=['map', 'bar-chart', 'table'], menu_icon='cast', default_index=0)
 
-    if selected == 'Mapa':
-        # SESSIONS STATES
-        if "selected_location" not in st.session_state:
-            st.session_state.selected_location = None
+if selected == 'Mapa':
+    # SESSIONS STATES
+    if "selected_location" not in st.session_state:
+        st.session_state.selected_location = None
 
-        col1, col2 = st.columns([4, 10])
-        with col1:
-            st.image(
-                "C:/Users/20391117579/Dropbox/CrimeApp/Multimedia/image.png",  width=350)
-        with col2:
-            st.title("Ciudad Autónoma de Buenos Aires")
+    col1, col2 = st.columns([4, 10])
+    with col1:
+        st.image(
+            "C:/Users/20391117579/Dropbox/CrimeApp/Multimedia/image.png",  width=350)
+    with col2:
+        st.title("Ciudad Autónoma de Buenos Aires")
 
-    # CONTAINER MODULO MAPA & DASHBOARD
-        m = modulo_mapa.folium_map()
-        if st.session_state.selected_location:
-            if st.button("Regresar"):
-                mapa = modulo_mapa.container_map(m)
-                st.session_state.selected_location = None
-            else:
-                dashboard = modulo_mapa.dashboard(_lat=st.session_state.selected_location[0], _lon=st.session_state.selected_location[1])
-        else:
+# CONTAINER MODULO MAPA & DASHBOARD
+    m = modulo_mapa.folium_map()
+    if st.session_state.selected_location:
+        if st.button("Regresar"):
             mapa = modulo_mapa.container_map(m)
+            st.session_state.selected_location = None
+        else:
+            dashboard = modulo_mapa.dashboard(_lat=st.session_state.selected_location[0], _lon=st.session_state.selected_location[1])
+    else:
+        mapa = modulo_mapa.container_map(m)
 
 
-    elif selected == 'Estadística':
-            st.subheader('Estadística')
-            st.write("Estadística")
+elif selected == 'Estadística':
+        st.subheader('Estadística')
+        st.write("Estadística")
 
-    # CONTAINER ESTADISTICA
-
-if __name__ == "__main__":
-    main()
-
-### MEJORAR DASHBOARD
+# CONTAINER ESTADISTICA
