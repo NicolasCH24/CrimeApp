7e57a26a01909636813dc5daa22f85f408be1393
Nicolas Chaparro <nchaparro@buenosaires.gob.ar>
2025-02-03 09:41:49 -0300

    Módulo optimizado.

--- AppCrimeStreamlit/Módulos/modulo_map.py
+++ AppCrimeStreamlit/Módulos/modulo_map.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 # AGENT
-from Módulos.clase_ai import DataframeAgent
+#from Módulos.clase_ai import DataframeAgent
 
 # MAPA - GRAFICOS
 import folium as fl
@@ -70,33 +70,9 @@ class ModuloMap:
         m = fl.Map(location=[-34.6083, -58.3712], zoom_start=12)
         m.add_child(AddMarkerOnClick())
         return m
-    
-    def graph_new_table(self, _lat, _lon):
-        df_table, comuna, barrio, hora, peligrosidad = self.clase_datos.get_actual_location_table(_lat, _lon)
 
-        tabla = st.dataframe(
-            df_table,
-            width=1100,
-            use_container_width=True,
-            column_config={
-                barrio: st.column_config.LineChartColumn(
-                    "Contactos por mes",
-                    help="Historial de contactos mensuales",
-                    y_min=0,
-                ),
-                "Franja horaria": st.column_config.LineChartColumn(
-                    "Contactos por franja horaria",
-                    help="Historial de contactos por franja horaria",
-                    y_min=0,
-                ),
-            },
-            hide_index=True,
-        )
-        
-        return tabla, comuna, barrio, peligrosidad
-
-    def graph_dashboard_elements(self, comuna, barrio, _lat, _lon):
-        df_map_box, tupla_mes, tupla_semana, delito_promedio, hechos_delito_promedio, df_locations = self.clase_datos.get_elements_dashbord(_lat, _lon, comuna, barrio)
+    def graph_dashboard_elements(self, peligrosidad, comuna, barrio, _lat, _lon):
+        df_map_box, tupla_mes, tupla_semana, delito_promedio, hechos_delito_promedio, df_locations = self.clase_datos.get_dashboard_data(_lat, _lon, comuna, barrio)
         df_bar = df_map_box
         df_map_box = df_map_box[['LATITUD', 'LONGITUD']][df_map_box['FRANJA_HORARIA'] == datetime.now().hour]
         # MAP BOX
@@ -149,11 +125,6 @@ class ModuloMap:
                 t=30,
                 pad=4
             ),
-            template={'data': {'indicator': [{
-                'title': {'text': "Speed"},
-                'mode': "number+delta+gauge",
-                'delta': {'reference': 90}}]
-            }}
         )
         
         # KPI SEMANA
@@ -178,11 +149,6 @@ class ModuloMap:
                 t=30,
                 pad=4
             ),
-            template={'data': {'indicator': [{
-                'title': {'text': "Speed"},
-                'mode': "number+delta+gauge",
-                'delta': {'reference': 90}}]
-            }}
         )
 
         # KPI DELITO PROMEDIO
@@ -206,12 +172,55 @@ class ModuloMap:
                 t=30,
                 pad=4
             ),
-            template={'data': {'indicator': [{
-                'title': {'text': "Speed"},
-                'mode': "number",
-                'delta': {'reference': 90}}]
-            }}
         )
+
+        # KPI PELIGROSIDAD
+        if peligrosidad <= 25:
+            color_barra = "rgba(255, 255, 153, 1)"  
+        elif peligrosidad <= 50:
+            color_barra = "rgba(255, 204, 102, 1)"  
+        elif peligrosidad <= 75:
+            color_barra = "rgba(255, 140, 0, 1)"
+        else:
+            color_barra = "rgba(255, 0, 0, 1)"
+
+        # Crear la figura
+        kpi_peligrosidad = go.Figure()
+
+        kpi_peligrosidad.add_trace(go.Indicator(
+            mode="number+gauge",
+            value=peligrosidad,
+            title = {'text':"% Peligrosidad",
+                     "font": {"size": 14, "color": "white"}},
+            number={'suffix': " %",
+                    'font':{'size':20, 'color':'white'}}, 
+            gauge={
+                'shape': "bullet",
+                'axis': {'range': [0, 100], 'visible': False}, 
+                'bar': {'color': color_barra},  
+                'steps': [  
+                    {'range': [0, 25], 'color': "rgba(255, 255, 153, 1)"},  
+                    {'range': [25, 50], 'color': "rgba(255, 204, 102, 1)"}, 
+                    {'range': [50, 75], 'color': "rgba(255, 140, 0, 1)"}, 
+                    {'range': [75, 100], 'color': "rgba(255, 0, 0, 1)"}  
+                ],
+            },
+            #domain={'x': [0.05, 0.5], 'y': [0.15, 0.35]} 
+        ))
+
+        kpi_peligrosidad.update_layout(
+            autosize=False,
+            width=950,
+            height=120,
+            margin=dict(
+                l=100,
+                r=0,
+                b=0,
+                t=30,
+                pad=4),
+        
+        )
+
         # GRAFICO DE BARRAS APILADAS
         df_grouped = df_bar.groupby(['TIPO_DELITO_DESC', 'FECHA']).agg({'CONTACTO_ID':'count'}).rename(columns={'CONTACTO_ID':'HECHOS'})
         df_grouped_totals = df_grouped.groupby('TIPO_DELITO_DESC')['HECHOS'].sum().reset_index()
@@ -272,18 +281,11 @@ class ModuloMap:
             showlegend=False
         )
 
-        # NARRATIVA
-        #_df = df_locations
-        #agent = DataframeAgent(_df)
-        #narrativa = agent.invoke("""Tu informacion está en: print(df[['Direcciones', 'Franja horaria promedio', 'Delito más frecuente', 'Hechos']])
-        #                            Retorna recomedacion para el ciudadano basandote en donde, cuando y detalles.
-        #                            """)['output']
-
-        return map_box, kpi_mes, kpi_semana, kpi_delito, fig_delitos, df_locations
+        return map_box, kpi_mes, kpi_semana, kpi_delito, kpi_peligrosidad, fig_delitos, df_locations
     
     def dashboard(self, _lat, _lon):
-        tabla, comuna, barrio, peligrosidad = self.graph_new_table(_lat, _lon)
-        map_box, kpi_mes, kpi_semana, kpi_delito, fig_delito, df_locations = self.graph_dashboard_elements(comuna, barrio, _lat, _lon)
+        comuna, barrio, hora, peligrosidad = self.clase_datos.get_location_data(_lat, _lon)
+        map_box, kpi_mes, kpi_semana, kpi_delito, kpi_peligrosidad, fig_delito, df_locations = self.graph_dashboard_elements(peligrosidad, comuna, barrio, _lat, _lon)
 
         with st.container(border=True):
             col1, col2 = st.columns([16, 10])
@@ -306,7 +308,7 @@ class ModuloMap:
             with col2:
                 st.markdown("#### Mapa del delito")
                 with st.container(border=True):
-                    st.metric(value=peligrosidad, label="Peligrosidad %")
+                    st.plotly_chart(kpi_peligrosidad)
                 with st.container(border=True):
                     st.pydeck_chart(map_box, use_container_width=True, height=690)
 
