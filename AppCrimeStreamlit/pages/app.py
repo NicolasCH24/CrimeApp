44e68ce31195d5439b787ba0bc58dcc49cbfbec7
Nicolas Chaparro <nchaparro@buenosaires.gob.ar>
2025-04-07 09:07:57 -0300

    Página informativa en mismo frame que el dashboard.

--- AppCrimeStreamlit/pages/app.py
+++ AppCrimeStreamlit/pages/app.py
@@ -2,6 +2,9 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+# TIEMPOS
+import time
+
 # MODULOS Y CLASES
 from Módulos.modulo_map import ModuloMap
 from Módulos.clase_graficos import Graficos
@@ -22,6 +25,12 @@ if selected == 'Mapa':
     # SESSIONS STATES
     if "selected_location" not in st.session_state:
         st.session_state.selected_location = None
+    
+    if "selected_hour" not in st.session_state:
+        st.session_state.selected_hour = None
+
+    if "hide_informativa" not in st.session_state:
+        st.session_state.hide_informativa = False
 
     col1, col2 = st.columns([4, 10])
     with col1:
@@ -30,20 +39,20 @@ if selected == 'Mapa':
     with col2:
         st.title("Ciudad Autónoma de Buenos Aires")
 
-# CONTAINER MODULO MAPA & DASHBOARD
+    # CONTAINER MODULO MAPA & DASHBOARD
     m = clase_graficos.folium_map()
-    if st.session_state.selected_location:
+    if st.session_state.selected_location and st.session_state.selected_hour:
         if st.button("Regresar"):
             mapa = modulo_mapa.container_main_map(m)
             st.session_state.selected_location = None
+            st.session_state.selected_hour = None
         else:
-            dashboard = modulo_mapa.dashboard(_lat=st.session_state.selected_location[0], _lon=st.session_state.selected_location[1])
+            informativa = modulo_mapa.container_informativo()
+            dashboard = modulo_mapa.dashboard(_lat=st.session_state.selected_location[0], _lon=st.session_state.selected_location[1], hora=st.session_state.selected_hour)
     else:
         mapa = modulo_mapa.container_main_map(m)
 
-
 elif selected == 'Estadística':
-        st.subheader('Estadística')
-        st.write("Estadística")
-
+    st.subheader('Estadística')
+    st.write("Estadística")
 # CONTAINER ESTADISTICA
